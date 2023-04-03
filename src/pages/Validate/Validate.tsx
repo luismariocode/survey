f6cedd347a4b7f9718146722d1ebd1495f28fe09
It's a me LuisMario <100161156+30ortiz@users.noreply.github.com>
2023-04-03 03:52:54 -0600

    Thankyou

--- src/pages/Validate/Validate.tsx
+++ src/pages/Validate/Validate.tsx
@@ -1,20 +1,57 @@
-import React from "react";
 
 const Validate = () => {
+
+  const entries: Record<string, unknown> = JSON.parse(localStorage.getItem("entries") || "{}");
+
+  const inputs = Object.entries(entries).map(([key, value]) => {
+    const name = key as string;
+    const val = value as string;
+    return (
+      <input
+        key={name}
+        type="hidden"
+        name={name}
+        value={val}
+      />
+    );
+  });
+
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault(); // evita el envío del formulario
+  //   const form = event.currentTarget;
+  //   // realiza la validación y envío del formulario aquí
+  // };
+
+
   return (
     <>
-      <section className="home">
-        <h1 className="home__titulo">
-          GRACIAS! <br />
-        </h1>
-        <span className="home__descripcion">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet,
-          molestias quia. Beatae esse, hic ipsum voluptas error tenetur!
-        </span>
+      <section className="thankyou">
+        <div className="thankyou__title">
+          <h1>¡GRACIAS!<br /></h1>
+          <span>
+            Ahora solo valida tus respuestas.
+          </span>
+        </div>
+
+        <form
+        className="validate"
+        action="https://docs.google.com/forms/d/e/1FAIpQLSeEK7Ey5QbK8629w-51e-MiuINgFy9eNlkkXHcmg3TqTQrqQQ/formResponse"
+        method="post"
+      // onSubmit={handleSubmit}
+      >
+
+        {inputs}
+        <div className="container-btn">
+          <button className="button--validar" type="submit">Validar tus Respuestas</button>
+        </div>
+        
+      </form>
       </section>
-      <button type="submit">Validar tus Respuestas</button>
+     
     </>
   );
 };
 
 export default Validate;
+
+// https://docs.google.com/forms/d/e/1FAIpQLSeEK7Ey5QbK8629w-51e-MiuINgFy9eNlkkXHcmg3TqTQrqQQ/viewform?usp=pp_url&entry.1591421491=fdgd&entry.23723034=gdfd&entry.402379595=Si
