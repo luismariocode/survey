f525c4c58a4c0a97b68753d41e512dd7ab243507
It's a me LuisMario <100161156+30ortiz@users.noreply.github.com>
2023-04-02 18:40:59 -0600

    fix

--- src/pages/Home/components/User.tsx
+++ src/pages/Home/components/User.tsx
@@ -77,7 +77,7 @@ const User: React.FC = () => {
                 handleKeyPress(event, handleSaveEntries, isValid)
               }
               placeholder="Nombre Completo"
-              autoComplete="off"
+              
               required
             />
           </div>
